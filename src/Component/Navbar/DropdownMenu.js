2034fbb701f5b3921eb988c612c6d658a0b0ccbf
AbhishekArora-accolite <abhishek.arora@accolitedigital.com>
2023-05-23 15:46:24 +0530

    minor change

---
+++ src/Component/Navbar/DropdownMenu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Navbar.css'
+
+function DropdownMenu() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default DropdownMenu;
--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
 import { AiOutlineMenu } from "react-icons/ai";
+import DropdownMenu from './DropdownMenu';
+import {NavLink} from 'react-router-dom'
+
 export default function Navbar() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
   return (
     <nav className='nav-container'>
       <img src={require('../../assets/logo.png')} className='logo'></img>
@@ -11,7 +18,13 @@ export default function Navbar() {
         <li className='navbar-link'>Pro</li>
       </ul>
       <button className='nav-btn'>Login</button>
-      <AiOutlineMenu className='menu-btn'/>
+        <AiOutlineMenu className='menu-btn' onClick={toggleMenu}/>
+        {isMenuOpen && (
+          <select className="navbar-menu">
+            <NavLink>Profile</NavLink>
+            <NavLink>Logout</NavLink>
+          </select>
+        )}
     </nav>
   )
 }
