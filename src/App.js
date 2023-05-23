8e7a972202165fdfbe031f5ab6add2f3238f16d3
AbhishekArora-accolite <abhishek.arora@accolitedigital.com>
2023-05-23 14:28:02 +0530

    navbar partially done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Practice from './Component/Practice/Practice';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import HomeScreen from './pages/HomeScreen';
 function App() {
   return (
     <div className="App">
-         <Practice/>
+        <HomeScreen />
+        {/* <Practice/> */}
     </div>
   );
 }
---
+++ src/pages/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Component/Navbar/Navbar'
+
+function HomeScreen() {
+  return (
+    <>
+        <Navbar/>  
+        
+    </>
+  )
+}
+
+export default HomeScreen
