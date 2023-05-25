cc1f68096307363c978475596bc44a02449700ab
Gobind2538 <gobind@accolitedigital.com>
2023-05-25 14:03:19 +0530

    accordian complete

--- src/Component/Practice/Practice.js
+++ src/Component/Practice/Practice.js
@@ -1,32 +1,34 @@
 import React from 'react'
 import './Practice.css'
 import Navbar from '../Navbar/Navbar'
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import Topic from './Topic';
 export default function Practice() {
+
+  let topics=["Arrays","Two Pointers","Window Sliding","Stack",
+              "Binary Search","Linked List","Trees","Heap/Priority Queue",
+              "Backtracking","Graph","1D DP","2D DP",
+              "Greedy","Bit Manipulation"]
+  
+function getProgressbar() {
+  const now = 25;
+  return <ProgressBar now={now} label={`${now}%` } />;
+}
+
   return (
     <>
     <Navbar/>
-    <div className='PracticeWrapper'>    
-    <div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        <div className='btn-div'>
-        <div className='col'>Arrays</div>
-        <div class="progress">
-        <div class="progress-bar" role="progressbar"  aria-valuenow={50} aria-valuemin="0" aria-valuemax="100">50%</div>
-        </div> 
-      </div>
-       
-      </button>
-    </h2>
-     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
+    <div className='PracticeWrapper'> 
+    {
+      topics.map((cur,index)=>{
+       return (
+        <Topic topicName={cur} Sr={index+1}/>
+        )
+      })
+    }   
   
-  <div class="accordion-item">
+  
+  {/* <div class="accordion-item">
     <h2 class="accordion-header" id="headingTwo">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
         Two Pointers
@@ -183,9 +185,9 @@ export default function Practice() {
         <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
       </div>
     </div>
-  </div>
+  </div> */}
 </div>
-</div>
+
     </>
   )
 }
